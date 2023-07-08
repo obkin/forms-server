3153ae89332373b7c610d8464fbc780fa48ed746
obkin <kapellwork@gmail.com>
2023-07-08 18:20:34 +0300

    added: working  NestJS server for tests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('user')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('get')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('postdata')
+  create(@Body() userData: { name: string; tel: string }) {
+    console.log(`name: ${userData.name}, phone: ${userData.tel}`);
   }
 }
