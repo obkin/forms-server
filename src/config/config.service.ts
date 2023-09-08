be2f448718dd781d89f2e6433f334630770053f6
obkin <kapellwork@gmail.com>
2023-09-08 12:20:51 +0300

    added: logger in ConfigService

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { IConfigService } from './config.service.interface';
 import { DotenvConfigOutput, DotenvParseOutput, config } from 'dotenv';
+import { LoggerService } from 'src/logger/logger.service';
 
 @Injectable()
 export class ConfigService implements IConfigService {
 	private envConfig: DotenvParseOutput;
-	constructor() {
+	constructor(private readonly loggerService: LoggerService) {
 		const result: DotenvConfigOutput = config();
 		if (result.error) {
-			console.log('[ConfigService] fail: configuration file (.env) not loaded or not found');
+			this.loggerService.error(
+				'[ConfigService]: configuration file (.env) not loaded or not found',
+			);
 		} else {
 			setTimeout(() => {
-				console.log('[ConfigService] success: configuration file (.env) loaded');
+				this.loggerService.log('[ConfigService]: configuration file (.env) loaded');
 			}, 5000);
 			this.envConfig = result.parsed as DotenvParseOutput;
 		}
