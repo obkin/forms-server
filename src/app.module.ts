420ea6a79979c02fa974ba9f5900480831fb22f6
obkin <kapellwork@gmail.com>
2023-09-14 10:53:36 +0300

    added: TypeormService, database connection using TypeormService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigService } from './config/config.service';
 import { DatabaseService } from './database/database.service';
 import { LoggerService } from './logger/logger.service';
+import { TypeormService } from './typeorm/typeorm.service';
+import { ConfigService } from './config/config.service';
 
 @Module({
 	imports: [],
 	controllers: [AppController],
-	providers: [AppService, ConfigService, DatabaseService, LoggerService],
+	providers: [AppService, ConfigService, DatabaseService, LoggerService, TypeormService],
 })
-export class AppModule {}
+export class AppModule {
+	constructor(private readonly databaseService: DatabaseService) {
+		this.databaseService.connect();
+	}
+}
--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { LoggerService } from 'src/logger/logger.service';
+import { TypeormService } from 'src/typeorm/typeorm.service';
 
 @Injectable()
 export class DatabaseService {
-	constructor(private readonly loggerService: LoggerService) {}
+	constructor(
+		private readonly loggerService: LoggerService,
+		private readonly typeormService: TypeormService,
+	) {}
 	async connect(): Promise<void> {
 		try {
 			// DB connect
+			await this.typeormService.PostgresDataSource.initialize();
 			this.loggerService.log('[DataBaseService]: database connected');
 		} catch (error) {
 			this.loggerService.error('[DataBaseService]: database was not connected:', error.message);
